a2ba7c0acdc088e0a108da83b46a22e30dd0d5ed
Tyra Banks <tyrabanks@MacBook-Pro-24994.local>
2024-04-14 18:38:55 -0400

    Deploying React app to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './App.css';
 
-const Video = () => (
-  <div className="video-container">
-    <video controls>
-      <source src="/logo/nlogo.mp4" type="video/mp4" />
-      Your browser does not support the video tag.
-    </video>
-  </div>
-);
+const Video = () => {
+  const videoRef = useRef(null);
+
+  const handleVideoClick = () => {
+    if (videoRef.current.paused) {
+      videoRef.current.play();
+    } else {
+      videoRef.current.pause();
+    }
+  };
+
+  return (
+    <div className="video-container" onClick={handleVideoClick}>
+      <video controls ref={videoRef}>
+      <source src="https://nicolecreative.github.io/logo/nlogo.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  );
+};
 
 function App() {
   return (
@@ -19,9 +31,9 @@ function App() {
         <Video />
       </main>
       <footer className="footer">
-      <div>
-      <a href="mailto:nicolebachman12@gmail.com" style={{ color: 'white', textDecoration: 'none' }}>nicolebachman12@gmail.com</a>
-  </div>
+        <div>
+          <a href="mailto:nicolebachman12@gmail.com" style={{ color: 'white', textDecoration: 'none' }}>nicolebachman12@gmail.com</a>
+        </div>
         <div>
           nicolebachman.com
         </div>
