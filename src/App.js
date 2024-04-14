e1b62a3bd7f4cd4b63a7b2df18efc5653dfff1b7
Tyra Banks <tyrabanks@MacBook-Pro-24994.local>
2024-04-14 18:05:48 -0400

    Deploying React app to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 const Video = () => (
   <div className="video-container">
-    <video autoPlay muted loop controls={false}>
+    <video muted loop controls>
       <source src="/logo/nlogo.mp4" type="video/mp4" />
       Your browser does not support the video tag.
     </video>
